49bfe9e9f6aa895b364448723f00f1c6c87b50e1
Arun6251 <158465347+Arun6251@users.noreply.github.com>
2024-02-19 13:10:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
+}
+darkBtn.onclick = function(){
+    darkBtn.classList.toogle("dark-btn-on");
+    document.body.classList.toogle("dark-theme");
+}
